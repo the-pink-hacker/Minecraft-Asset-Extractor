3559286d473b4b64300f26eac0024b8271ac8700
Ryan Garrett <RyanGar46@gmail.com>
2021-03-11 01:14:04 -0500

    Update Compile.ps1

--- Compile.ps1
+++ Compile.ps1
@@ -1,5 +1,6 @@
 pyinstaller Minecraft_Asset_Extractor.py, Extract.py
-Copy-Item "icon.png" -Destination "dist\Minecraft_Asset_Extractor\icon.png"
-Copy-Item "default_settings.ini" -Destination "dist\Minecraft_Asset_Extractor\default_settings.ini"
-Copy-Item "LICENSE" -Destination "dist\Minecraft_Asset_Extractor\LICENSE.txt"
-Copy-Item "README.md" -Destination "dist\Minecraft_Asset_Extractor\README.md"
+Copy-Item icon.png -Destination dist\Minecraft_Asset_Extractor\icon.png
+Copy-Item default_settings.ini -Destination dist\Minecraft_Asset_Extractor\default_settings.ini
+Copy-Item LICENSE -Destination dist\Minecraft_Asset_Extractor\LICENSE.txt
+Copy-Item README.md -Destination dist\Minecraft_Asset_Extractor\README.md
+Invoke-Item dist\Minecraft_Asset_Extractor\
