b6c435f31cf6826fd62ac105e2c3f962d6f5ef61
Ryan Garrett <RyanGar46@gmail.com>
2021-07-23 04:20:57 -0400

    Update Compile.ps1
    I spent so long trying to get one file to work and this is all I had to do.

--- Compile.ps1
+++ Compile.ps1
@@ -1,7 +1,6 @@
-pyinstaller --icon=icon.ico Minecraft_Asset_Extractor.py, Extract.py
-Copy-Item config.json -Destination dist\Minecraft_Asset_Extractor\config.json
-Copy-Item icon.ico -Destination dist\Minecraft_Asset_Extractor\icon.ico
-Copy-Item default_settings.ini -Destination dist\Minecraft_Asset_Extractor\default_settings.ini
-Copy-Item LICENSE -Destination dist\Minecraft_Asset_Extractor\LICENSE.txt
-Copy-Item README.md -Destination dist\Minecraft_Asset_Extractor\README.md
-Invoke-Item dist\Minecraft_Asset_Extractor\
+pyinstaller --onefile --noupx --icon=icon.ico Minecraft_Asset_Extractor.py, Extract.py
+Copy-Item config.json -Destination dist\config.json
+Copy-Item icon.ico -Destination dist\icon.ico
+Copy-Item default_settings.ini -Destination dist\default_settings.ini
+Copy-Item LICENSE -Destination dist\LICENSE.txt
+Copy-Item README.md -Destination dist\README.md
